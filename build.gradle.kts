3e454d46254204724333d9ec1b0d6b7c441f26b8
Sydney Renee <sydney@solace.ofharmony.ai>
2025-12-05 23:50:44 -0800

    fix: update developer email address

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ mavenPublishing {
             developer {
                 id.set("sydneyrenee")
                 name.set("Sydney Renee")
-                email.set("sydney@thesolace.ai")
+                email.set("sydney@solace.ofharmony.ai")
                 url.set("https://github.com/sydneyrenee")
             }
         }

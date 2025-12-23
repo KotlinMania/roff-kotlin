7bf1d7d5312e7b2b73df17122cf5f712e279679e
Sydney Renee <sydney@solace.ofharmony.ai>
2025-12-22 22:56:06 -0800

    Update Kotlin to 2.3.0 and add more platforms

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,81 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
 plugins {
-    kotlin("multiplatform") version "2.2.10"
+    kotlin("multiplatform") version "2.3.0"
+    id("com.android.kotlin.multiplatform.library") version "8.6.0"
     id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 group = "io.github.kotlinmania"
 version = "0.1.0"
 
-repositories {
-    mavenCentral()
+// Setup Android SDK location and licenses automatically
+val sdkDir = file(".android-sdk")
+val licensesDir = sdkDir.resolve("licenses")
+if (!licensesDir.exists()) licensesDir.mkdirs()
+val licenseFile = licensesDir.resolve("android-sdk-license")
+if (!licenseFile.exists()) {
+    licenseFile.writeText(
+        """
+        8933bad161af4178b1185d1a37fbf41ea5269c55
+        d56f5187479451eabf01fb74abc367c344559d7b
+        24333f8a63b6825ea9c5514f83c2829b004d1fee
+        """.trimIndent()
+    )
+}
+val localProperties: File? = rootProject.file("local.properties")
+if (!localProperties?.exists()!!) {
+    localProperties.writeText("sdk.dir=${sdkDir.absolutePath}")
 }
 
 kotlin {
     applyDefaultHierarchyTemplate()
 
-    // Native targets
-    macosArm64()
-    macosX64()
+    val xcf = XCFramework("Roff")
+
+    macosArm64 {
+        binaries.framework {
+            baseName = "Roff"
+            xcf.add(this)
+        }
+    }
+    macosX64 {
+        binaries.framework {
+            baseName = "Roff"
+            xcf.add(this)
+        }
+    }
     linuxX64()
     mingwX64()
+    iosArm64 {
+        binaries.framework {
+            baseName = "Roff"
+            xcf.add(this)
+        }
+    }
+    iosX64 {
+        binaries.framework {
+            baseName = "Roff"
+            xcf.add(this)
+        }
+    }
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = "Roff"
+            xcf.add(this)
+        }
+    }
+    js {
+        browser()
+        nodejs()
+    }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        nodejs()
+    }
 
     sourceSets {
         val commonMain by getting
@@ -27,10 +85,20 @@ kotlin {
             }
         }
     }
+
+    jvmToolchain(21)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "io.github.kotlinmania.roff"
+        compileSdk = 34
+        minSdk = 24
+    }
 }
 
 mavenPublishing {
-    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
 
     coordinates(group.toString(), "roff-kotlin", version.toString())

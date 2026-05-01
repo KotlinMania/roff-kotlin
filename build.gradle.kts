44c437bb187c4c62c68bec23e7a4d920ca47120b
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-01 01:19:41 -0700

    Align Kotlin targets and CI toolchains

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@ import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest
 
 plugins {
     kotlin("multiplatform") version "2.3.20"
-    id("com.android.kotlin.multiplatform.library") version "8.6.0"
+    id("com.android.kotlin.multiplatform.library") version "9.2.0"
     id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 group = "io.github.kotlinmania"
-version = "0.1.3"
+version = "0.1.4"
 
 val androidSdkDir: String? =
     providers.environmentVariable("ANDROID_SDK_ROOT").orNull
@@ -28,6 +28,10 @@ if (enableAndroid) {
 
 kotlin {
     applyDefaultHierarchyTemplate()
+
+    compilerOptions {
+        allWarningsAsErrors.set(true)
+    }
 
     val xcf = XCFramework("Roff")
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    plugins { kotlin("jvm") version "2.3.0" }
+    plugins { kotlin("jvm") version "2.3.20" }
 }
 
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0" }
